dce78c46d3ad621f06f2c23f91811f20ed2a1363
Sushant Kumar <sushantkumar2025@gmail.com>
2025-01-01 00:26:57 +0530

    Added alternate total order support by adding comparators to util functions.

--- src/main/java/com/algorithms/sorting/utils/Utils.java
+++ src/main/java/com/algorithms/sorting/utils/Utils.java
@@ -1,8 +1,14 @@
 package main.java.com.algorithms.sorting.utils;
 
+import java.util.Comparator;
+
 public class Utils {
     public static <T extends Comparable<T>> boolean less(T v, T w) {
         return v.compareTo(w) < 0;
+    }
+
+    public static<T> boolean less(Comparator<? super T> comparator, T v, T w){
+        return comparator.compare(v, w) < 0;
     }
 
     public static <T> void swap(T[] a, int i, int j) {
@@ -26,4 +32,22 @@ public class Utils {
         }
         return true;
     }
+
+    public static <T> boolean isSorted(Comparator<? super T> comparator, T[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(comparator, a[i], a[i - 1]))
+                return false;
+        }
+        return true;
+    }
+
+    public static <T> boolean isSorted(Comparator<? super T> comparator, T[] a, int low, int high) {
+        for (int i = low + 1; i < high; i++) {
+            if (less(comparator, a[i], a[i - 1]))
+                return false;
+        }
+        return true;
+    }
+
+
 }
